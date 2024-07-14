f9583b57ee106e632317e42cd1b04baa4afbcfc1
lucas <lsgalante12@gmail.com>
2024-07-13 23:35:32 -0400

    Add new options and a separatornto the IM menu. Somehow I broke the formatting but it actually looks ok.

--- scripts/python/im_menu_utils.py
+++ scripts/python/im_menu_utils.py
@@ -1,20 +1,7 @@
 import hou
 
-def rename_pane_tab():
-
-    tab = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
-    print(tab)
-    name = tab.name()
-    new_name = hou.ui.readInput(
-      message='Rename current tab ' + name,
-      buttons=('Ok', 'Cancel'),
-      default_choice=1,
-      close_choice=1)
-    if new_name[0] == 1:
-        return
-    else:
-        tab.setName(new_name[1])
-
+def reload_viewport_color_schemes():
+    hou.ui.reloadViewportColorSchemes()
 
 def toggle_im_view():
     viewer = hou.ui.paneTabOfType(hou.paneTabType.SceneViewer)
@@ -22,15 +9,23 @@ def toggle_im_view():
         node = viewer.pwd()
         viewer_type = node.childTypeCategory().name()
 
-        if viewer_type == 'Object':
-            viewer.setCurrentState('im_view_obj')
-
-        elif viewer_type == 'Sop':
-            viewer.setCurrentState('im_view_sop')
-
+        if viewer_type == "Object" or viewer_type == "Sop":
+            viewer.setCurrentState("im_view")
         else:
-            print('IM View currently needs a SOP or OBJ type scene viewer.')
+            print("Only Sop or Object contexts are available.")
 
     else:
-        print('No available scene viewer.')
+        print("No available scene viewer.")
         return
+
+def reload_im_view():
+    hou.ui.reloadViewerState("im_view")
+
+def update_main_menu():
+    hou.ui.updateMainMenuBar()
+
+def restart_to_this_file():
+    x=1
+
+def open_color_chooser():
+    hou.ui.selectColor()
