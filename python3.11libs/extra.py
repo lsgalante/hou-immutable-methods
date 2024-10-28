144d6c700792ab443f6f58930fca2e7092935162
l_s_galante <lsgalante12@gmail.com>
2024-10-28 08:11:54 -0400

    work on node theme

--- python3.11libs/extra.py
+++ python3.11libs/extra.py
@@ -1,8 +1,10 @@
-import hou
-import math
+import hou, labutils, math, os
 from canvaseventtypes import *
 import nodegraphdisplay as display
 import nodegraphview as view
+from PySide2.QtCore import Qt, QRect
+from PySide2.QtGui import QColor, QCursor, QPen, QPixmap
+from PySide2.QtWidgets import QGraphicsScene, QGraphicsView
 
 
 def action(uievent):
@@ -12,6 +14,8 @@ def action(uievent):
 
     if isinstance(uievent, KeyboardEvent) and\
         uievent.eventtype == 'keyhit':
+
+#        print(uievent.key)
 
         editor       = uievent.editor
         node         = editor.currentNode()
@@ -24,63 +28,86 @@ def action(uievent):
         view_xform   = [0, 0]
         view_step    = 160
 
-        print (key)
-
-        if key == 'Ctrl+Shift+H':          # Move Node Left
+        # Move node left
+        if key == 'Ctrl+Shift+H':
             if round ((x%1), 1) <= 0.5:
                 x = math.floor(x) - 0.5
-            else: x = math.ceil(x) - 0.5
-
+            else:
+                x = math.ceil(x) - 0.5
+        # Layout nodes
         elif key == "Ctrl+Shift+A":
             node.parent().layoutChildren(horizontal_spacing=5, vertical_spacing=5), 
-
-        elif key == 'Ctrl+Shift+J':        # Move Node Down
+        # Move node down
+        elif key == 'Ctrl+Shift+J':
             if round ((y%1), 2) > 0.85:
                 y = math.ceil(y) - 0.15
-            else: y = math.floor(y) - 0.15
-
-        elif key == 'Ctrl+Shift+K':        # Move Node Up
+            else:
+                y = math.floor(y) - 0.15
+        # Move node up
+        elif key == 'Ctrl+Shift+K':
             if round((y%1), 2) < 0.85:
                 y = math.floor(y) + 0.85
-            else: y = math.ceil(y) + 0.85
-
-        elif key == 'Ctrl+Shift+L':        # Move Node Right
+            else:
+                y = math.ceil(y) + 0.85
+        # Move node right
+        elif key == 'Ctrl+Shift+L':
             if round( (x%1), 1) >= 0.5:
                 x = math.ceil(x) + 0.5
-            else: x = math.floor(x) + 0.5
-
-        elif key == "Shift+D":             # Insert Dot
+            else:
+                x = math.floor(x) + 0.5
+        # Place dot
+        elif key == "Shift+D":
             selected = hou.selectedNodes()
             if len(selected) == 1:
                 context = node.parent()
-                dot     = context.createNetworkDot()
-                dot.setInput (node)
+                dot = context.createNetworkDot()
+                dot.setInput(node)
                 cursor_pos = editor.cursorPosition()
                 dot.setPosition(cursor_pos)
-
         elif key == "Shift+G":
             if editor.getPref("gridmode") == "0":
                 editor.setPref("gridmode", "2")
             else:
                 editor.setPref("gridmode", "0")
-
-        elif key == 'H':                   # Pan Left
-            view_xform[0] = -view_step * zoom_amt
-        elif key == 'J':                   # Pan Down
-            view_xform[1] = -view_step * zoom_amt
-        elif key == 'K':                   # Pan Up
-            view_xform[1] = view_step * zoom_amt
-        elif key == 'L':                   # Pan Right
-            view_xform[0] = view_step * zoom_amt
-
-        elif key == "M":                   # Toggle Update Mode
+        # Pan left
+        elif key == 'H': view_xform[0] = -view_step * zoom_amt
+        # Pan down
+        elif key == 'J': view_xform[1] = -view_step * zoom_amt
+        # Pan up
+        elif key == 'K': view_xform[1] = view_step * zoom_amt
+        # Pan right
+        elif key == 'L': view_xform[0] = view_step * zoom_amt
+        # Change update mode
+        elif key == "M":
             if hou.updateModeSetting() == hou.updateMode.Manual:
-                hou.setUpdateMode (hou.updateMode.AutoUpdate)
-
+                hou.setUpdateMode(hou.updateMode.AutoUpdate)
             else:
-                hou.setUpdateMode (hou.updateMode.Manual)
+                hou.setUpdateMode(hou.updateMode.Manual)
 
-        node.setPosition ((x, y))
-        visible_bounds = editor.visibleBounds ()
-        visible_bounds.translate (hou.Vector2(view_xform[0], view_xform[1]))
-        editor.setVisibleBounds (visible_bounds)
+        node.setPosition( (x, y) )
+        xform = hou.Vector2(view_xform[0], view_xform[1])
+        visible_bounds = editor.visibleBounds()
+        visible_bounds.translate(xform)
+        editor.setVisibleBounds(visible_bounds)
+
+        # Draw outline
+        draw_outline(uievent)
+
+
+def draw_outline(uievent):
+    editor = uievent.editor
+    image = hou.NetworkImage()
+    image.setPath('$HIP/drawings/bg.png')
+    image.setRect(hou.BoundingRect(0, 0, 1, 1))
+
+    screen_space = editor.screenBounds()
+    visible_items = editor.networkItemsInBox(screen_space.min(), screen_space.max(), for_select=True)
+    images = []
+    for item in visible_items:
+        pos = item[0].position()
+        pos = hou.Vector2(pos.x()-0.025, pos.y()-0.35)
+        pos2 = hou.Vector2(pos.x()+1.05, pos.y()+1)
+        image.setRect(hou.BoundingRect(pos, pos2))
+        images.append(image)
+
+    editor.setBackgroundImages([image])
